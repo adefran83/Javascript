c56036c27cd4ca20dbfb7d135e777d13519da25b
Anthony DeFrancesco <adefran83@gmail.com>
2013-03-24 19:18:00 -0400

    Added new files for assignment2e

---
+++ TitanMusicStore/js/store.js
@@ -0,0 +1,24 @@
+function getAlbumsByArtist (artistId) {
+	if (typeof artistId == 'number') {
+		// do some stuff
+		return results;
+	} else {
+		alert('Please enter a valid artist ID.')
+	}
+}
+function getAlbumsByCategory (categoryId) {
+	if (typeof categoryid == 'number') {
+		//do some stuff
+		return results;
+	} else {
+		alert('Please enter a valid category ID.')
+	}
+}
+function getAllAlbums () {
+	// do stuff to retrieve all albums
+	return results;
+}
+function getAllCategories() {
+	// do stuff to retrieve all categories
+	return results;
+}
